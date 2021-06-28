cd9d05eb02b779a3919758629f1ddae1968531bf
dhineshak <dhineshkarthi.9@gmail.com>
2021-06-28 23:03:09 +0530

    uibuild

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
 const cors = require('cors')
+const mongoose = require('mongoose')
+const Contact = require('./models/contacts')
 
 app.use(express.static('build'))
 app.use(express.json())
@@ -12,81 +15,104 @@ const requestLogger = (request, response, next) => {
   console.log('Body:  ', request.body)
   next()
 }
-
 app.use(requestLogger)
+ 
+// const generateId = () => {
+//   let newId = 0
+//   const id = contacts.map(contact => contact.id)
+//   while(newId === 0 || id.indexOf(newId) !== -1){
+//     newId = Math.floor(Math.random()*1000) + 1;
+//   }
+//   return newId
+// }
 
-let contacts = [
-    {
-      name: "Arto Hellas",
-      number: "040-123456",
-      id: 1
-    },
-    {
-      name: "Ada Lovelace",
-      number: "39-44-5323523",
-      id: 2
-    },
-    {
-      name: "Dan Abramov",
-      number: "12-43-234345",
-      id: 3
-    },
-    {
-      name: "Dad",
-      number: "39-23-6423122",
-      id: 4
+app.get('/api/persons',(request, response, next) => {
+  Contact.find({}).then(contacts => {
+    response.json(contacts)
+    // mongoose.connection.close()
+  }).catch(error => next(error))
+})
+
+app.get('/api/persons/:id',(request, response, next) => {
+  Contact.findById(request.params.id).then(result => {
+    if(result){
+      response.json(result)
+    }else{
+      return response.status(404).end()
     }
-  ]
-
-const generateId = () => {
-  let newId = 0
-  const id = contacts.map(contact => contact.id)
-  while(newId === 0 || id.indexOf(newId) !== -1){
-    newId = Math.floor(Math.random()*1000) + 1;
-  }
-  return newId
-}
-
-app.get('/api/persons',(request, response) => {
-  response.json(contacts)
+    // mongoose.connection.close()
+  }).catch(error => next(error))
 })
 
-app.get('/api/persons/:id',(request, response) => {
-  const id = Number(request.params.id)
-  const contact = contacts.filter(contact => contact.id === id)
-  if(contact.length === 0){
-    return response.status(404).end()
-  }
-  response.json(contact)
-})
-
-app.post('/api/persons/',(request, response) => {
+app.post('/api/persons',(request, response, next) => {
   const newContact = request.body
-  if(!(newContact) || !(newContact.name)){
-    return response.status(400).json({error:'Name cannot be Empty'})
+  if(!(newContact) || !(newContact.name) || !(newContact.number)){
+    // mongoose.connection.close()
+    return response.status(400).json({error:'Name or number cannot be Empty'})
   }
-  if(!(newContact.number)){
-    return response.status(400).json({error:'Number cannot be Empty'})
-  }
-  const name = contacts.filter(contact => newContact.name === contact.name)
-  
-  if(name.length > 0){
-    return response.status(400).json({error:'Name must be unique'})
-  }
-  newContact.id = generateId()
-  contacts.push(newContact)
-  response.json(newContact)
+  Contact.find({name:newContact.name}).then(result => {
+    if(result.length != 0){
+      const editContact = {
+        name: newContact.name,
+        number: newContact.number,
+      }
+      Contact.findByIdAndUpdate(request.params.id,editContact,{new:true}).then(result => {
+          response.json(result)
+        // mongoose.connection.close()
+      }).catch(error => next(error))
+    }
+  })
+  const contact = new Contact({
+    name: newContact.name,
+    number: newContact.number,
+  })
+  contact.save().then(result => {
+    response.json(result)
+    // mongoose.connection.close()
+  }).catch(error => next(error))
 })
 
-app.delete('/api/persons/:id',(request, response) => {
-  const id = Number(request.params.id)
-  contacts = contacts.filter(contact => contact.id != id)
-  return response.status(204).end()
+app.put('/api/persons/:id',(request, response, next) => {
+  const newContact = request.body
+  const editContact = {
+    name: newContact.name,
+    number: newContact.number,
+  }
+  Contact.findByIdAndUpdate(request.params.id,editContact,{new:true}).then(result => {
+    response.json(result)
+    // mongoose.connection.close()
+  }).catch(error => next(error))
+})
+
+app.delete('/api/persons/:id',(request, response, next) => {
+  Contact.findByIdAndRemove(request.params.id).then(result => {
+      response.status(204).end()
+    // mongoose.connection.close()
+  }).catch(error => next(error))
 })
 
 app.get('/api/info',(request, response) => { 
-  response.send(`<div><p>Phonebook has info for ${contacts.length} people</p><p>${new Date()}</p></div>`)
+  Contact.find({}).then(contacts => {
+    response.send(`<div><p>Phonebook has info for ${contacts.length} people</p><p>${new Date()}</p></div>`)
+    mongoose.connection.close()
+  })
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }else if(error.name === 'ValidationError'){
+    return response.status(400).send({ error: error.message })
+  }
+  next(error)
+}
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT,()=>{
